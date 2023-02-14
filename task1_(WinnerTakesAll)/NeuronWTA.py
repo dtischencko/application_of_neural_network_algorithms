b061dcf0af1ce5c9a7be68d5df335f81883e34c9
Dmitry Tischencko <90377536+dtischencko@users.noreply.github.com>
2023-02-14 16:05:51 +0300

    wta fix

--- task1_(WinnerTakesAll)/NeuronWTA.py
+++ task1_(WinnerTakesAll)/NeuronWTA.py
@@ -3,13 +3,9 @@ import numpy as np
 
 class Neuron:
 
-    last_ind = np.NaN
-    id_ne = np.NaN
-
     def __init__(self, id_ne):
+        self.last_indu = None
         self.id_ne = id_ne
 
     def set_indu_method(self, xs, ws, iter_num):
-        self.last_ind = ws.iloc[self.id_ne].T.dot(xs.iloc[iter_num])
-
-
+        self.last_indu = ws.iloc[self.id_ne].T.dot(xs.iloc[iter_num])
